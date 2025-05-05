38faa43e8256aabf0db76903a2767387dc284f65
Himanshu Mehndiratta <himanshu@Himanshus-MacBook-Air.local>
2025-05-06 00:16:55 +0530

    Allow req

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+app.use(cors());
  
 // apis
 app.use("/api/v1/media", mediaRoute);
